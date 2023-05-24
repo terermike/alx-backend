5c8f8cb24790f4bfb31f4357f390f504eda0a8bb
Terer <terermike3@gmail.com>
2023-05-24 12:57:25 +0300

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@ Module documentation: This is a basic Flask app.
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
