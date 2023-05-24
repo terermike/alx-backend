9ee1c225adc9931acfd005b9ecdfcc5aecca7d96
Terer <terermike3@gmail.com>
2023-05-24 12:53:15 +0300

    Task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module documentation: This is a basic Flask app.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Function documentation: Renders the index.html template.
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
